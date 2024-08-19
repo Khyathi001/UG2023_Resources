6595538d1e9c8875d28745974bab163815e77916
Khyathi Basireddy <143736080+Khyathi001@users.noreply.github.com>
2024-08-19 20:28:54 +0530

    Delete sem_2/CSO directory

--- sem_2/CSO/Question_bank/Q2/q2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-long long int NoOfOnes(long long int N);
-int main(){
-    long long int N;
-    scanf("%lld",&N);
-    long long int ans = NoOfOnes(N);
-    if(ans%2!=0) printf("TRUE\n");      // if it contains odd number of ones print true
-    else printf("FALSE\n");             // if it contains even number of ones print false
-}
